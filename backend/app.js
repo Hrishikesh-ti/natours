eaa0c51b20bbfaaffb697efe7e2b2d02e28c5c65
Hrishikesh A. Tiwari <hrishikeshtiwari@Hrishikeshs-MacBook-Air.local>
2023-02-22 13:45:34 +0530

    optimize

--- backend/app.js
+++ backend/app.js
@@ -47,9 +47,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(helmet());
 
 // Development logging
-// if (process.env.NODE_ENV === 'development') {
-// }
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Limit requests from same API
 const limiter = rateLimit({
