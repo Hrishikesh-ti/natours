6872556d075ddd3aa78d673f4044d37f0a404e8e
Hrishikesh A. Tiwari <hrishikeshtiwari@Hrishikeshs-MacBook-Air.local>
2023-05-07 12:55:39 +0530

    initial

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -79,23 +79,28 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  // 1) Check if email and password exist
-  // if (!email || !password) {
-  //   return next(new AppError('Please provide email and password!', 400));
-  // }
-  // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email }).select('+password');
+    // 1) Check if email and password exist
+    // if (!email || !password) {
+    //   return next(new AppError('Please provide email and password!', 400));
+    // }
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
 
-  if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password', 401));
+    }
+
+    // 3) If everything ok, send token to client
+    createSendToken(user, 200, res);
+  } catch (error) {
+    console.log(error);
+    return next(new AppError(error, 401));
   }
-
-  // 3) If everything ok, send token to client
-  createSendToken(user, 200, res);
-});
+};
 
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
